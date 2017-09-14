315389c54be3102099004f44a9ad5c54437e0e51
mhwangc <mhwangc@berkeley.edu>
2017-09-13 19:54:52 -0700

    finish hw1

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,13 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    a.map! {|elem| elem.to_i + 2}
+    a.keep_if {|elem| elem % 2 == 0}
+    a.uniq!
+    a.delete_if {|elem| elem > 10}
+    x = 0
+    a.each {|elem| x += elem}
+    x
   end
 end
 
